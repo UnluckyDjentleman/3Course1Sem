dbfc976e1d4295038b5f58d796baee9b5ed45eeb
dalvhikki <v.goroshchenja@gmail.com>
2023-09-24 14:08:54 +0300

    forogot to comment async/await in 03b-01

--- PSCP/Labs/lab3b/03b-01.js
+++ PSCP/Labs/lab3b/03b-01.js
@@ -10,10 +10,10 @@ const promiseA=new Promise((resolve,reject)=>{
 
 firstJob().then().catch(error=>{console.log("Error: ",error)}).finally(()=>{console.log("Task 1 is done")});
 
-async function firstJob(){
+/*async function firstJob(){
     console.log("firstJob is running")
     let result=await promiseA;
     console.log(result)
 }
 
-firstJob().catch(error=>{console.log("Error: ",error)}).finally(()=>{console.log("Task 1 is done")});
+firstJob().catch(error=>{console.log("Error: ",error)}).finally(()=>{console.log("Task 1 is done")});*/
