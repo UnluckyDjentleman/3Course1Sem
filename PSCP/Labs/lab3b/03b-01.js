4039a040e79e9dcc51fe18a7c944f031296613c9
dalvhikki <v.goroshchenja@gmail.com>
2023-09-24 13:55:56 +0300

    add lab3b pscpa

---
+++ PSCP/Labs/lab3b/03b-01.js
@@ -0,0 +1,24 @@
+firstJob=()=>{
+    console.log("firstJob is running")
+    return promiseA.then((result)=>{console.log(result)})
+}
+const promiseA=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('Hello World')
+    },2000)
+})
+
+firstJob().then().catch(error=>{console.log("Error: ",error)}).finally(()=>{console.log("Task 1 is done")});
+
+/*async function firstJob(){
+    console.log("firstJob is running")
+    let result=await promiseA;
+    console.log(result)
+}
+const promiseA=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve('Hello World')
+    },2000)
+})
+
+firstJob();*/
